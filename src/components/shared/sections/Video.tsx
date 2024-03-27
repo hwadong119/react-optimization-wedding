b482c25cd86b18332d24badb4ccbc7d548959662
hwadong <jeonghwayun0119@naver.com>
2024-03-27 12:34:10 +0900

    feat: add Video component, implement introduction video

--- src/components/shared/sections/Video.tsx
+++ src/components/shared/sections/Video.tsx
@@ -7,7 +7,16 @@ const cx = classNames.bind(styles)
 
 function Video() {
   return (
-    <Section>Video</Section>
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true} // 자동 재생
+        muted={true} // 무음
+        loop={true} // 반복 재생
+        poster="/assets/poster.jpg" // 재생 전에 표시될 포스터 이미지
+      >
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
   )
 }
 
